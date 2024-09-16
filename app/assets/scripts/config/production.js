4738b21e56da87284ab55ee291adc14e39adaf8a
Abdallah Hossam <abdallahossam1998@gmail.com>
2024-09-16 18:27:41 +0300

    Update production API root URL

--- app/assets/scripts/config/production.js
+++ app/assets/scripts/config/production.js
@@ -7,7 +7,7 @@ module.exports = {
   environment: 'production',
   auth0_token: process.env.AUTH0_TOKEN,
   auth0_namespace: process.env.AUTH0_NAMESPACE,
-  api_root: process.env.API_ROOT,
+  api_root: 'https://map-egypt.cartologic.com/api',
   consoleMessage: logo
 };
 
